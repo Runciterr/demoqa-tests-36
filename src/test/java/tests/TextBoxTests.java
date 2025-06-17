f73e703cfc876e7e96efccd633cc03fae8614b6f
Runciter <unformatmail@gmail.com>
2025-06-17 22:12:19 +0300

    added tests

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,41 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @AfterAll
+    static void tearDown() {
+        closeWebDriver();
+    }
+
+
+    @Test
+    void textBoxPositiveTest() {
+        open("/text-box");
+        $("#userName").setValue("Denisovich");
+        $("#userEmail").setValue("DenBel@yandex.ru");
+        $("#currentAddress").setValue("Korolev");
+        $("#permanentAddress").setValue("Gorod");
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("Denisovich"));
+        $("#output #email").shouldHave(text("DenBel@yandex.ru"));
+        $("#output #currentAddress").shouldHave(text("Korolev"));
+        $("#output #permanentAddress").shouldHave(text("Gorod"));
+    }
+
+}
